c23b12c5e623d6d6da34da79cfa6a7c70df76611
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-06 13:44:01 +0200

    Added SVG social icons

--- site/snippets/social-bar.php
+++ site/snippets/social-bar.php
@@ -1,5 +1,20 @@
 <ul class="social__bar">
-  <li><a href="<?php echo $site->instagram()->html() ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-  <li><a href="<?php echo $site->facebook()->html() ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-  <li><a href="<?php echo $site->twitter()->html() ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+  <li>
+    <a href="<?php echo $site->instagram()->html() ?>">
+      <svg width="32" height="32"><use xlink:href="#i:instagram"/></svg>
+      Instagram
+    </a>
+  </li>
+  <li>
+    <a href="<?php echo $site->facebook()->html() ?>">
+      <svg width="32" height="32"><use xlink:href="#i:facebook"/></svg>
+      Facebook
+    </a>
+  </li>
+  <li>
+    <a href="<?php echo $site->twitter()->html() ?>">
+      <svg width="32" height="32"><use xlink:href="#i:twitter"/></svg>
+      Twitter
+    </a>
+  </li>
 </ul>
