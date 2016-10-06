158ecab276f3af0e093ae0f79513b7c130fdf5f9
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-06 16:07:10 +0200

    menu push

---
+++ site/snippets/menu.php
@@ -0,0 +1,30 @@
+<nav class="primary-nav">
+  <a href="#navigation" class="nav-trigger">
+    <span>
+      <em aria-hidden="true"></em>
+    </span>
+  </a> <!-- .nav-trigger -->
+  <ul id="navigation">
+    <li>
+      <a href="<?php echo url('/'); ?>" class="logo">
+        <img src="<?php echo url('assets/images/logo.gif') ?>" alt="Logo">
+      </a>
+    </li>
+    <?php foreach($pages->visible() as $p): ?>
+      <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a></li>
+    <?php endforeach ?>
+    <li class="busbtn">
+      <a href="<?php echo url('contact'); ?>">
+        <i class="fa fa-bus" aria-hidden="true"></i>
+        Request the science bus
+      </a>
+    </li>
+  </ul>
+</nav> <!-- .primary-nav -->
+<nav class="secondary-nav">
+  <?php snippet('social-bar') ?>
+  <?php snippet('search') ?>
+</nav> <!-- .secondary-nav -->
+
+
+
