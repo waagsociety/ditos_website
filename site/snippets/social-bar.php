1d151298f4b6261efb16748184969fc84184c585
Martin Risseeuw <martin@martinr.nl>
2016-10-06 11:58:26 +0200

    social update

---
+++ site/snippets/social-bar.php
@@ -0,0 +1,5 @@
+<ul class="social__bar">
+  <li><a href="<?php echo $site->instagram()->html() ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+  <li><a href="<?php echo $site->facebook()->html() ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+  <li><a href="<?php echo $site->twitter()->html() ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+</ul>
