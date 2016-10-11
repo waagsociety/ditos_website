80b35f5936408becf744cceab9eb8600502c2890
Martin Risseeuw <martin@martinr.nl>
2016-10-11 09:29:21 +0200

    account update

---
+++ site/accounts/nathan.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: nathan
+firstname: Nathan
+lastname: ""
+email: nathan@waag.org
+password: >
+  $2a$10$bKZ9ovsA2ApDIePBrAte3.IT/MaYyQDwc6Vt/QRUV1o9Wr.xm9F2K
+language: en
+role: admin
