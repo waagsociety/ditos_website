a81552cbd4c24547501217eda424b9153a719b76
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-12 14:17:37 +0200

    merge

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -8,9 +8,6 @@ password: >
   $2a$10$bKZ9ovsA2ApDIePBrAte3.IT/MaYyQDwc6Vt/QRUV1o9Wr.xm9F2K
 language: en
 role: admin
-<<<<<<< HEAD
 history:
   - calendar
   - book-the-bus
-=======
->>>>>>> parent of e8d9576... Push before implementing Kirby calendar plugin
