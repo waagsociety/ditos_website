a629efdeed2c9b0ae540178b540755d0644a4aaf
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-13 09:50:14 +0200

    Featured links to book-the-bus

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,30 +1,38 @@
 <nav class="primary-nav">
+ 
   <a href="#navigation" class="nav-trigger">
     <span>
       <em aria-hidden="true"></em>
     </span>
-  </a> <!-- .nav-trigger -->
+  </a>
+
   <ul id="navigation">
+    
     <li>
       <a href="<?php echo url('/'); ?>" class="logo">
         <img src="<?php echo url('assets/images/logo.gif') ?>" alt="Logo">
       </a>
     </li>
+
     <?php foreach($pages->visible() as $p): ?>
       <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a></li>
     <?php endforeach ?>
+
     <li class="busbtn">
-      <a href="<?php echo url('contact'); ?>" class="icon labeled">
+      <a href="<?php echo url('book-the-bus'); ?>" class="icon labeled">
         <svg width="32" height="32"><use xlink:href="#i:bus"/></svg>
         Request the science bus
       </a>
     </li>
+
   </ul>
-</nav> <!-- .primary-nav -->
+
+</nav>
+
 <nav class="secondary-nav">
   <?php snippet('social-bar') ?>
   <?php snippet('search') ?>
-</nav> <!-- .secondary-nav -->
+</nav>
 
 
 
