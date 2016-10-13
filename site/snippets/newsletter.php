037500f12b0ff21c455d8cff277a9aa47478adf6
Martin Risseeuw <martin@martinr.nl>
2016-10-13 15:56:44 +0200

    added logo hover

--- site/snippets/newsletter.php
+++ site/snippets/newsletter.php
@@ -1,12 +1,14 @@
-<div class="form-wrapper container left">
-  <form class="form">
+<div id="mc_embed_signup" class="form-wrapper container left">
+  <form action="//ucl.us7.list-manage.com/subscribe/post?u=44f4fdd80f93a247bc56da002&amp;id=f48fcef17d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form validate" target="_blank" novalidate>
     <label class="label" for="email"><i class="fa fa-envelope-o" aria-hidden="true"></i> Sign up for the newsletter</label>
-    <input type="email" id="email" class="email" name="email" placeholder="Enter your email address">
-    <button type="submit" class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> submit</button>
+    
+    <input autocomplete="off" type="email" id="mce-EMAIL" class="required email" name="EMAIL" placeholder="Enter your email address">
+    <button id="mc-embedded-subscribe" type="submit" class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> submit</button>
     <div class="loading"></div>
   </form>
-
-  <div data-type="message" class="response response-error">Ops! Error message here</div>
-  <div data-type="message" class="response-success"><p>Great! Success message here</p></div>
+  <div data-type="message" class="response response-error" id="mce-error-response">Ops! Error message here</div>
+  <div data-type="message" class="response-success" id="mce-success-response"><p>Great! Success message here</p></div>
   <div data-type="message" class="response response-notification">Hey! Notification message here</div>
 </div>
+
+
--- src/js/logo.js
+++ src/js/logo.js
@@ -1,4 +1,3 @@
-
 !function(){
 
   var logo = document.getElementById('svg-logo');
