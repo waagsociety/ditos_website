b448103edc6c189951007b07161d1a7672353967
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-13 16:00:57 +0200

    map box merge

--- site/templates/events.php
+++ site/templates/events.php
@@ -8,17 +8,13 @@
   <div class="flex flex__wrap" id="list">
  
     <section>
-<<<<<<< HEAD
 
-      <div id="map" hidden></div>
+      <?php snippet('mapbox') ?>
       
       <div class="event__list">
       <?php snippet('events-list') ?>
       </div>
 
-=======
-      <?php snippet('mapbox') ?>
->>>>>>> origin/master
     </section>
 
     <aside><?php snippet('events-filters') ?></aside>  
