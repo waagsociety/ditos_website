20ef2e46681dd9bea131e2ef2fb55b7332f9906c
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-10-17 16:26:49 +0200

    area around bus can no longer be clicked

--- site/snippets/event-information.php
+++ site/snippets/event-information.php
@@ -3,23 +3,34 @@
 </header>
 
 <ul class="event__details full">
-    
+  
+  <li>
+    <h1>Host</h1>
+    <p>De Waag</p>
+  </li>
+
   <li>
     <svg viewBox="0 0 32 32"><use xlink:href="#i:calendar"/></svg> 
     <time datetime="<?php echo $page->date('c') ?>">
       <?php echo $page->date('d-m-Y') ?>      
     </time>
+    <br>
+    <a href="http://maps.google.com/?q=<?php echo $q ?>" target="_blank">
+      <button type="button">Add to Calendar</button>
+    </a>
   </li>
 
   <li>
-    <a href="/">
-      <svg viewBox="0 0 32 32"><use xlink:href="#i:location"/></svg>
-      <?php if (strlen($page->venue()->html()) > 0) { 
-        echo $page->venue()->html().'<br>';
-      } ?>
-      <?php echo $page->address() ?><br>
-      <?php echo $page->city().'<br>'.$page->country() ?>
-      <button type="button">Open in Google Maps</button>
+    <svg viewBox="0 0 32 32"><use xlink:href="#i:location"/></svg>
+    <?php $q = $page->address().' '.$page->city().' '.$page->country() ?>    
+    <?php if (strlen($page->venue()->html()) > 0) { 
+      echo $page->venue().'<br>';
+    } ?>
+    <?php echo $page->address() ?><br>
+    <?php echo $page->city().', '.$page->country() ?>
+    <br>
+    <a href="http://maps.google.com/?q=<?php echo $q ?>" target="_blank">
+      <button type="button">View in Google Maps</button>
     </a>
   </li>
 
