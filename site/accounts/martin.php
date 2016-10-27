b8c8f9a448972726a0a34558d514ef7a332aabe7
Martin Risseeuw <martin@martinr.nl>
2016-10-27 16:13:26 +0200

    updated OG meta data

--- site/accounts/martin.php
+++ site/accounts/martin.php
@@ -7,8 +7,8 @@ email: martin@waag.org
 language: en
 role: admin
 history:
+  - blog
   - blog/doing-it-together-science
   - events
   - blog/doing-it-together
   - events/another-antwerp-event
-  - about/deliverables
--- site/snippets/seo.php
+++ site/snippets/seo.php
@@ -1,16 +1,14 @@
-
-  <?php if($page->seotitle()->isNotEmpty()): ?>
-    <meta property="og:title" content="<?php echo $page->seotitle()->html() ?>" />
-  <? else: ?>
-    <meta property="og:title" content="<?php echo $page->title()->html() ?>" />
-  <?php endif ?>
-  <meta property="og:url" content="<?php echo $page->url() ?>" />
-  
-  <?php if($page->postimage()->isNotEmpty()): ?>
-    <meta property="og:image" content="<?php echo $page->postimage() ?>" />
-  <? else: ?>
-    <meta property="og:image" content="aap.jpg" />
-  <?php endif ?>
-
-
 <meta property="og:site_name" content="<?php echo $site->title()->html() ?>" />
+<?php if($page->seotitle()->isNotEmpty()): ?>
+  <meta property="og:title" content="<?php echo $page->seotitle()->html() ?>" />
+<? else: ?>
+  <meta property="og:title" content="<?php echo $page->title()->html() ?>" />
+<?php endif ?>
+<meta property="og:url" content="<?php echo $page->url() ?>" />
+
+<?php if($page->postimage()->isNotEmpty()): ?>
+  <?php $postimage = $page->postimage()->toFile() ?>
+  <meta property="og:image" content="<?php echo $postimage->url() ?>" />
+<? else: ?>
+  <meta property="og:image" content="<?php echo url('assets/images/a.jpg') ?>" />
+<?php endif ?>
