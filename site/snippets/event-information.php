b5146a19d2260ee09cb3d8a0c8a2e69a94762a94
Martin Risseeuw <martin@martinr.nl>
2016-11-15 12:07:59 +0100

    styling events sidebar

--- site/snippets/event-information.php
+++ site/snippets/event-information.php
@@ -22,44 +22,48 @@
   <?php if ($activity) : ?>
   <li>
     <header>What</header>
-    <?php echo $activity->title() ?>
-    <?php if (count($tags)) : ?>
-    <ul class="tags">
-      <?php foreach ($tags as $value) : ?>
-      <li><a href="">#<?= $value ?></a>
-      <?php endforeach ?>
-    </ul>
-    <?php endif ?>
+    <section>
+      <?php echo $activity->title() ?>
+      <?php if (count($tags)) : ?>
+      <ul class="tags">
+        <?php foreach ($tags as $value) : ?>
+        <li><a href="">#<?= $value ?></a>
+        <?php endforeach ?>
+      </ul>
+      <?php endif ?>
+    </section>
   </li>
   <?php endif ?>
 
   <li>
     <header>When</header>
-    <?php
-      $date = $page->date('l d.m.Y');
-      $time = $page->time();
-      
-      $date_end = $page->date_end();
-      $date_end = str_replace('-', '/', $date_end);
-      $date_end_time = strtotime($date_end);
-      $date_end = date('Y-m-d', $date_end_time);
-      $date_end = $date_end == $page->date('Y-m-d') ? null : $date_end;
+    <section>
+      <?php
+        $date = $page->date('l d.m.Y');
+        $time = $page->time();
+        
+        $date_end = $page->date_end();
+        $date_end = str_replace('-', '/', $date_end);
+        $date_end_time = strtotime($date_end);
+        $date_end = date('Y-m-d', $date_end_time);
+        $date_end = $date_end == $page->date('Y-m-d') ? null : $date_end;
 
-      $time_end = strlen($page->time_end()) > 0 ? $page->time_end() : null;
-    ?>
-    <time datetime="<?php echo $page->date('c').' '.$time ?>">
-      <?php echo $date ?> at <?php echo $time ?>
-    </time>
-    <?php if ($date_end || $time_end) : ?>
-      to
-      <time>
-        <?php if ($date_end) { echo date('l d.m.Y', $date_end_time).' at '; } ?> 
-        <?php echo $time_end ?>
+        $time_end = strlen($page->time_end()) > 0 ? $page->time_end() : null;
+      ?>
+      <time datetime="<?php echo $page->date('c').' '.$time ?>">
+        <?php echo $date ?> at <?php echo $time ?>
       </time>
-    <?php endif ?>
-    <a href="<?php echo $page->url() ?>/format:ics/">
-      <button type="button">Add to Calendar</button>
-    </a>
+      <?php if ($date_end || $time_end) : ?>
+        to
+        <time>
+          <?php if ($date_end) { echo date('l d.m.Y', $date_end_time).' at '; } ?> 
+          <?php echo $time_end ?>
+        </time>
+      <?php endif ?>
+      <a href="<?php echo $page->url() ?>/format:ics/">
+        <button type="button" class="btn-1">Add to Calendar</button>
+      </a>
+    </section>
   </li>
   
   <?php $location = $pages->find('locations')->find($page->location()) ?>
@@ -69,15 +73,16 @@
     <header>
       <!-- <svg viewBox="0 0 32 32"><use xlink:href="#i:location"/></svg> -->
       Where
-    </header>       
-    <?php echo $location->title()?><br>
-    <?php echo $location->address() ?><br>
-    <?php echo $location->city().', '.$location->country() ?>
+    </header>   
+    <section>    
+      <?php echo $location->title()?><br>
+      <?php echo $location->address() ?><br>
+      <?php echo $location->city().', '.$location->country() ?>
 
-    <a href="http://maps.google.com/?q=<?php echo $q ?>" target="_blank">
-      <button type="button">View in Google Maps</button>
-    </a>
-
+      <a href="http://maps.google.com/?q=<?php echo $q ?>" target="_blank">
+        <button type="button" class="btn-1">View in Google Maps</button>
+      </a>
+      </section>
   </li>
   <?php endif ?>
 
@@ -85,9 +90,11 @@
   <?php $links = $page->link()->toStructure() ?>
   <?php if (count($links)) : ?>
   <li>
-    <?php foreach ($links as $link) : ?>
-      <a class="btn" href="<?=  $link->url() ?>"><?= $link->label() ?></a>
-    <?php endforeach ?>
+    <section>
+      <?php foreach ($links as $link) : ?>
+        <a class="btn btn-1" href="<?=  $link->url() ?>"><?= $link->label() ?></a>
+      <?php endforeach ?>
+    </section>
   </li>
   <?php endif ?>
 
