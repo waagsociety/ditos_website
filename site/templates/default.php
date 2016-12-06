8ef83cc98d6ffba75108fe2f0a449dbd1e28e172
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-12-06 16:26:24 +0100

    added logo to partner page

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,6 +4,7 @@
     <section>
       <?php snippet('standard-header') ?>
       <div class="text">
+        <?php e($page->logo()->isNotEmpty(), '<img class="central" width="240" src="'.$page->logo()->toFile()->url().'">') ?>
         <?php echo kirbytext($page->text()) ?>
       </div>
     </section>
