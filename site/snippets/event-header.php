4f152dfed6f70377bb3b498ac00782e41032d52e
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-12-06 14:48:38 +0100

    made custom header for event-item

---
+++ site/snippets/event-header.php
@@ -0,0 +1,22 @@
+<header class="blog__header">
+  
+  <?php $postimage = $page->postimage()->toFile() ?>
+  <?php if ($postimage) : ?>
+  <figure>
+    <img src="<?php echo $postimage->url() ?>" width="100%" />
+    <?php if (strlen($postimage->copyright()) > 0) : ?>
+      <figcaption>
+        &copy; <?php echo $postimage->copyright() ?>
+      </figcaption>
+    <?php endif ?>
+  </figure>
+  <?php endif ?>
+
+  <h1><?php echo $page->title() ?></h1>
+  
+  <?php
+    $author = $page->author();
+    $date = $page->date();
+    $tags = !!trim($page->tags()) ? explode(',', $page->tags()) : false;
+  ?>
+</header>
