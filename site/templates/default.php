2a857a4842b83a765e52d1b4fe2688bbc83502cd
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-12-07 10:54:42 +0100

    Fixed a bug in default template logo

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,11 +1,17 @@
+
 <?php snippet('header') ?>
+
 <main class="main__content">
   <div class="flex flex__wrap">
     <section>
       <?php snippet('standard-header') ?>
       <div class="text">
-        <?php e($page->logo()->isNotEmpty(), '<img class="central" width="240" src="'.$page->logo()->toFile()->url().'">') ?>
-        <?php echo kirbytext($page->text()) ?>
+      <?php 
+        echo $page->logo()->isNotEmpty() 
+          ? '<img class="central" width="240" src="'.$page->logo()->toFile()->url().'">'
+          : '';
+        echo kirbytext($page->text()) 
+      ?>
       </div>
     </section>
     <aside>
