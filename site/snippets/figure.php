4c2e485993ade118f089f6712412f84b97846589
Nathan Uphoff <nathan@Administrators-MacBook-Pro.local>
2016-12-07 12:13:59 +0100

    made improvements for licensing images

---
+++ site/snippets/figure.php
@@ -0,0 +1,49 @@
+<?php 
+// $image should be passed as the second argument of the snippet method
+$image = $image ? $image->toFile() : false;
+
+$licenseIndex = [
+  'CC0' => [
+    'title' => 'Public Domain',
+    'href' => 'https://creativecommons.org/publicdomain/zero/1.0/'
+  ],
+  'BY' => [
+    'title' => 'Attribution required',
+    'href' => 'https://creativecommons.org/licenses/by/2.0/'
+  ],
+];
+
+if ($image) : ?>
+  
+  <?php
+
+    $url = $image->url();
+    $width = $image->width();
+    $height = $image->height();
+    $caption = $image->caption()->isNotEmpty() ? $image->caption() : null;
+    $author = $image->copyright();
+
+    $licenseValue = trim($image->license()->html());
+    $licenseKey = strlen($licenseValue) > 0 ? $licenseValue : 'CC0';
+    $license = $licenseIndex[$licenseKey];
+
+  ?>
+
+  <figure>
+
+    <img src="<?= $url ?>" width="<?= $width ?>" height="<?= $height ?>">
+    
+    <footer>
+      <a class="license <?= $licenseKey ?>" href="<?= $license['href'] ?>" title="License: <?= $license['title'] ?>" target="_blank">
+        <?= $license['title'] ?>        
+      </a>
+      <?= $author->html() ?>
+    </footer>
+
+    <?php if ($caption) : ?>
+    <figcaption><?= $caption->kirbytext() ?></figcaption>
+    <?php endif ?>
+
+  </figure>
+
+<?php endif ?>
--- site/snippets/standard-header.php
+++ site/snippets/standard-header.php
@@ -1,14 +1,4 @@
 <header class="blog__header">
-  <?php $postimage = $page->postimage()->toFile() ?>
-  <?php if ($postimage) : ?>
-  <figure>
-    <img src="<?php echo $postimage->url() ?>" width="100%" />
-    <?php if (strlen($postimage->copyright()) > 0) : ?>
-      <figcaption>
-        &copy; <?php echo $postimage->copyright() ?>
-      </figcaption>
-    <?php endif ?>
-  </figure>
-  <?php endif ?>
+  <?php snippet('figure', ['image' => $page->postimage()]) ?>
   <h1><?php echo $page->title() ?></h1>
 </header>
