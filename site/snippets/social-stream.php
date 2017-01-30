73568fab6560c92246be03359973f6699cc2e581
Martin Risseeuw <martin@martinr.nl>
2017-01-30 13:21:13 +0100

    style update

--- site/snippets/social-stream.php
+++ site/snippets/social-stream.php
@@ -1,2 +1,5 @@
+<section class="social-intro">
+  <h2>Join us via #DITscience</h2>
+</section>
 <script src="https://assets.juicer.io/embed.js" type="text/javascript"></script>
 <ul class="juicer-feed" data-feed-id="ditos"></ul>
