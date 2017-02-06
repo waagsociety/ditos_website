5622a4d9b480d6eb1b367ed8845665672d91c71b
Martin Risseeuw <martin@martinr.nl>
2017-02-06 16:00:33 +0100

    responsive style update because of slide

--- site/snippets/slider.php
+++ site/snippets/slider.php
@@ -3,25 +3,22 @@
 ?>
 
 <section class="slider">
-<nav class="slider-nav">
+  <nav class="slider-nav">
+    <?php foreach($slides as $key => $slide): ?>
+      <button class="bullet" data-slide-id="<?= $key ?>" href="#slide-<?= $key ?>"></button>
+    <?php endforeach ?>
+  </nav>
   <?php foreach($slides as $key => $slide): ?>
-    <a class="bullet" href="#slide-<?= $key ?>"></a>
+    <figure id="#slide-<?= $key ?>" class="item" style="background-image: url(<?php echo $page->contentURL() ?>/<?php echo $slide['image'] ?>);">
+      <img src="http://localhost:8888/content/gmo-genetic-modification-glow-dark.jpg" />
+      <figcaption class="caption">
+        <?php echo kirbytext($slide['content']) ?>
+      </figcaption>
+    </figure>
   <?php endforeach ?>
-</nav>
-<?php foreach($slides as $key => $slide): ?>
-  <figure id="#slide-<?= $key ?>" class="item" style="background-image: url(<?php echo $page->contentURL() ?>/<?php echo $slide['image'] ?>);">
-    <img src="http://localhost:8888/content/gmo-genetic-modification-glow-dark.jpg" />
-    <figcaption class="caption">
-      <?php echo kirbytext($slide['content']) ?>
-    </figcaption>
-  </figure>
-<?php endforeach ?>
-
-
 </section>
 
 <script type="text/javascript">
-
   function loadSlides() {
     var slider = document.querySelector('.slider')
     var current = 0
@@ -40,16 +37,26 @@
       pause = false
     }
 
+    bullets.forEach(function(bullet) {
+      bullet.addEventListener('click', function(element){
+        document.querySelector('.slider .item.active').classList.remove('active')
+        document.querySelector('.slider .bullet.active').classList.remove('active')
+        current = parseInt(this.dataset.slideId)
+        items[current].classList.toggle('active');
+        bullets[current].classList.toggle('active');
+      });
+    });
+
     setInterval(function() {
-      console.log('current slide id', current)
       if (pause != true ) {
         document.querySelector('.slider .item.active').classList.remove('active')
         document.querySelector('.slider .bullet.active').classList.remove('active')
         current = (current != items.length - 1) ? current + 1 : 0;
+        console.log(current)
         items[current].classList.toggle('active');
         bullets[current].classList.toggle('active');
       }
-    }, 5000);
+    }, 10000);
   }
 
   loadSlides();
