6ea598106af28417a708de255e9aea560c247eba
Martin Risseeuw <martin@martinr.nl>
2017-02-09 09:57:13 +0100

    Fixed newsletter functionality in Safari

--- site/snippets/newsletter.php
+++ site/snippets/newsletter.php
@@ -1,11 +1,12 @@
+<!-- Begin MailChimp Signup Form -->
 <div id="mc_embed_signup" class="form-wrapper container left">
-  <form action="//ucl.us7.list-manage.com/subscribe/post?u=44f4fdd80f93a247bc56da002&amp;id=f48fcef17d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form validate" target="_blank" novalidate>
-    <label class="label" for="email"><i class="fa fa-envelope-o" aria-hidden="true"></i> Sign up for the newsletter</label>
-    
-    <input autocomplete="off" type="email" id="mce-EMAIL" class="required email" name="EMAIL" placeholder="Enter your email address">
-    <button id="mc-embedded-subscribe" type="submit" class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> submit</button>
-    <div class="loading"></div>
-  </form>
-  <div data-type="message" class="response response-error" id="mce-error-response">Ops! Error message here</div>
-  <div data-type="message" class="response-success" id="mce-success-response"><p>Great! Success message here</p></div>
+<form action="//togetherscience.us7.list-manage.com/subscribe/post?u=44f4fdd80f93a247bc56da002&amp;id=f48fcef17d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="form validate" target="_blank" novalidate>
+  <label class="label" for="email"><i class="fa fa-envelope-o" aria-hidden="true"></i> Sign up for the newsletter</label>
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+  <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_44f4fdd80f93a247bc56da002_f48fcef17d" tabindex="-1" value=""></div>
+  <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">Subscribe</button> 
+</form>
 </div>
+
+<!--End mc_embed_signup-->
