dd1e43f6c82ba50acbf51839ee1535b54ec7edb8
Nathan Uphoff <nathan@Nathan.local>
2017-07-25 11:58:06 +0200

    changed order

--- site/templates/deliverables.php
+++ site/templates/deliverables.php
@@ -19,10 +19,10 @@
         <?php if($items->pagination()->hasPages()): ?>
           <nav class="pagination">
             <?php if($items->pagination()->hasNextPage()): ?>
-            <a class="btn btn-3 left" href="<?php echo $items->pagination()->nextPageURL() ?>">Next</a>
+            <a class="btn btn-3 left" href="<?php echo $items->pagination()->nextPageURL() ?>">Previous</a>
             <?php endif ?>
             <?php if($items->pagination()->hasPrevPage()): ?>
-            <a class="btn btn-3 right" href="<?php echo $items->pagination()->prevPageURL() ?>">Previous</a>
+            <a class="btn btn-3 right" href="<?php echo $items->pagination()->prevPageURL() ?>">Next</a>
             <?php endif ?>
           </nav>
         <?php endif ?>
