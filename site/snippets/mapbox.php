6d2204e86c13eefa20a33a033ff7e7f1a50f9151
Nathan Uphoff <nathan@Nathan.local>
2017-10-31 12:39:40 +0100

    update

--- site/snippets/mapbox.php
+++ site/snippets/mapbox.php
@@ -9,24 +9,28 @@ $features = array();
 foreach($data as $article) {
   $location = $pages->find('locations')->find($article->location());
   if ($location) {
+    if ($location->location() != ",") {
+      $coords = explode(",", $location->location());
       $features[] = array(
-      'type'  => 'Feature',
-      'properties' => array(
-        'url'   => (string)$article->url(),
-        'title' => (string)$article->title(),
-        'introsentence'  => (string)$article->description(),
-        'date'  => (string)$article->date('d.m.Y'),
-        'time'  => (string)$article->time(),
-        'address'  => (string)$location->title()
-      ),
-      'geometry' => array(
-        'type'   => 'Point',
-        'coordinates' => array(
-          floatval(explode(",",(string)$location->location())[1]),
-          floatval(explode(",",(string)$location->location())[0])
+        'type'  => 'Feature',
+        'properties' => array(
+          'url'   => (string)$article->url(),
+          'title' => (string)$article->title(),
+          'introsentence'  => (string)$article->description(),
+          'date'  => (string)$article->date('d.m.Y'),
+          'time'  => (string)$article->time(),
+          'address'  => (string)$location->title()
+        ),
+        'geometry' => array(
+          'type'   => 'Point',
+          'coordinates' => array(
+            floatval($coords[1]),
+            floatval($coords[0])
+          )
         )
-      )
-    );
+      );
+    }
+    else echo 'console.warn("location ‘'.$article->location().'’ for event ‘'.$article->title().'’ has no coordinates");';
   }
   else echo 'console.warn("location ‘'.$article->location().'’ for event ‘'.$article->title().'’ does not exist");';
 }
@@ -41,13 +45,13 @@ echo 'var geojson = '.json_encode($geojson).';';
 ?>
 </script>
 <script type="text/javascript">
-  geojson.features = geojson.features.filter(function(feature) { 
-    var coords = feature.geometry.coordinates
-    if (!coords[0] && !coords[1]) {
-      console.warn('no coordinates for location ‘' + feature.properties.address + '’ at ‘' + feature.properties.title + '’')
-    }
-    return coords[0] || coords[1]
-  })
+  // geojson.features = geojson.features.filter(function(feature) { 
+  //   var coords = feature.geometry.coordinates
+  //   if (!coords[0] && !coords[1]) {
+  //     console.warn('no coordinates for location ‘' + feature.properties.address + '’ at ‘' + feature.properties.title + '’')
+  //   }
+  //   return coords[0] || coords[1]
+  // })
   
   console.info(JSON.stringify(geojson, null, 2))
   document.addEventListener("DOMContentLoaded", function(event) {
