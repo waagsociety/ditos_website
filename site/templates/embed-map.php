4f09f878ebbe037ff1c852cbdb3488f91f14fab5
Nathan Uphoff <nathan@Nathan.local>
2017-11-07 10:07:18 +0100

    php fixes

--- site/templates/embed-map.php
+++ site/templates/embed-map.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title><? $site->title() ?></title>
+  <title><?php echo $site->title(); ?></title>
   <style type="text/css">
   * {
     margin: 0;
@@ -11,10 +11,12 @@
     outline: 0;
     border: none;
   }
+  body {
+    overflow: hidden;
+  }
   </style>
 </head>
 <body>
-  <iframe width="100%" height="100%" src="<? echo $page->source() ?>">
+  <iframe width="100%" height="100%" src="<?php echo $page->source(); ?>">
 </body>
 </html>
-<? print_r($page) ?>
