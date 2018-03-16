87ad522567f26eaade0f3ce965d1a35e90076505
Taco <taco@Tacos-MacBook-Pro-2.local>
2018-03-16 17:25:52 +0100

    move javascript inline

--- site/fields/doaselect/doaselect.php
+++ site/fields/doaselect/doaselect.php
@@ -2,12 +2,6 @@
 /* just a standard select field, but with some extra javascript glue */
 class DoaselectField extends BaseField {
 
-    //we just add some javascript magic here
-    static public $assets = array(
-        'js' => array('doa_fill.js'),
-        'css' => array()
-    );
-
   public function __construct() {
 
     $this->type    = 'doaselect';
@@ -26,6 +20,59 @@ class DoaselectField extends BaseField {
       'selected' => $selected
     ));
   }
+
+  //override body to append extra stuff
+  public function content()
+  {
+      $content = parent::content();
+      //add the script inline instead of above, because otherwise it won't work the first time
+      $js = <<<JAVASCRIPT
+console.log("yo");
+var eventTypes = ["Workshops","Workshops","Workshops","Workshops","Interactive Travelling Exhibition","Workshops","Conference / Seminar","Conference / Seminar","Workshops","Science Cafes / Public Screenings","Science Cafes / Public Screenings","Workshops","Interactive Travelling Exhibition","Workshops","Conference / Seminar","Conference / Seminar","Online activities","Interactive Travelling Exhibition","Workshops","Online activities","Science Cafes / Public Screenings","Science Cafes / Public Screenings","Science Cafes / Public Screenings","Workshops","Delegations visits","Online activities","Workshops","Workshops","Workshops","Workshops","Workshops","Science Cafes / Public Screenings","Workshops","Online activities","Workshops","Workshops","Workshops","Online activities","Science Cafes / Public Screenings","Online activities","Online activities","Online activities","Workshops","Workshops","Science Cafes / Public Screenings","Workshops","Interactive Travelling Exhibition","Workshops","Workshops","Science Cafes / Public Screenings","Conference / Seminar","Conference / Seminar","Workshops","Interactive Travelling Exhibition","Workshops","Online activities","Science Cafes / Public Screenings","Interactive Travelling Exhibition","Science Cafes / Public Screenings","Science Cafes / Public Screenings","Workshops","Online activities","Workshops","Workshops","Interactive Travelling Exhibition","Interactive Travelling Exhibition","Conference / Seminar","Delegations visits","Interactive Travelling Exhibition","Online activities","Science Cafes / Public Screenings","Workshops","Online activities","Interactive Travelling Exhibition","Online activities","Online activities","Interactive Travelling Exhibition"];
+
+var workpackages = ["1","1","1","1","1","2","1","1","1","1","1","3","2","2","2","3","1","2","2","1","1","2","2","3","4","1","1","2","2","2","2","2","2","3","4","4","4","1","3","3","1","3","3","1","3","4","2","1","1","1","4","1","4","2","2","3","2","3","2","1","4","2","2","2","2","3","","","","","","","3","2","1","2","2"];
+
+function registerNameChange()
+{
+    var nameField = $('#form-field-event_name')[0];
+    var wpField = $('#form-field-work_package')[0];
+    var typeField = $('#form-field-activity')[0];
+
+    nameField.addEventListener("change", function() {
+
+        var index = nameField.selectedIndex - 1;//the first value is a label, not a value
+        
+        //update the two readonly fields via the mapping
+        if(index > -1){
+           
+            //parse the wp number
+            var wp = parseInt(workpackages[index]);
+            var wp_index = -1;//the default empty value
+            if(!isNaN(wp)){
+                wp_index = wp - 1;//index starts at 0     
+            }
+            wpField.value = wp_index;
+            
+            var eventType = eventTypes[index];
+            typeField.value = eventType;
+        }
+        else //the empty selector
+        {
+            wpField.value = "";
+            typeField.value = "";
+        }
+    });
+}
+
+$(function() {//document ready
+    registerNameChange();
+    console.log("registered");
+});
+JAVASCRIPT;
+
+      $content->append("<script>" . $js . "</script>");
+      return $content;
+  } 
 
   public function input() {
 
@@ -59,10 +106,11 @@ class DoaselectField extends BaseField {
     $inner->addClass('selectbox-wrapper');
     $inner->append($select);
 
+
     $wrapper = new Brick('div');
     $wrapper->addClass('input input-with-selectbox');
     $wrapper->append($inner);
-
+    
     if($this->readonly()) {
       $wrapper->addClass('input-is-readonly');
     } else {
